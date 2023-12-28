ced5c0211b496cfaa0c1813b65f97f6f9137a627
Rajat Rajput <rajat.contact.rajat@gmail.com>
2023-12-28 14:15:13 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rajat Rajput",
+  title: "Web Designer & Software Engineer",
+  email: "rajat.rajput@studentambassadors.com",
+  gitHub: "rajatuiwebdev",
+  instagram: "rajatrajput.dev",
+  linkedIn: "rajatrajput2004",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/scenery2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "background with plants";
 
 /**
  * Sort description that expands on your title on the Home component.
@@ -34,7 +34,7 @@ const skillsList = [
   "Web design",
   "User experience",
   "Inclusive design",
-  "Focus group testing",
+  "Software Development",
   "Mobile user interfaces",
   "Graphic design",
 ];
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "This is my portfolio website.";
 
 const About = () => {
   return (
